e3871a1a78b4631e55d2692c3d8b0c27c36d4373
elenaa <elena.torralbo@opendeusto.es>
2024-11-08 11:37:47 +0100

    amen

---
+++ src/main/java/com/example/strava_tw1/StravaTw1Application.java
@@ -0,0 +1,13 @@
+package com.example.strava_tw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaTw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaTw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/strava_tw1/StravaTw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strava_tw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaTw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
